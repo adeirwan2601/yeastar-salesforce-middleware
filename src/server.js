fa85d56e2ea8abc8f7ca96f0edfdd5f920a4744b
adeirwan2601 <adeirwan0902@gmail.com>
2026-01-06 17:16:07 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const http = require('http');
 const app = require('./app');
 const socketService = require('./services/socket.service');
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer(app);
 socketService.init(server);
 
-server.listen(3000, () => {
-  console.log('Middleware running on port 3000');
+server.listen(PORT, () => {
+  console.log(`🚀 Middleware running on port ${PORT}`);
 });
